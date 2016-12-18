76712fdddb60d99755823b8102b3cffc0c8f93c9
Gui Talarico <gtalarico@gmail.com>
2016-12-18 01:11:34 -0500

    Working csharp addin

---
+++ addin/src/watcher/SandwichWatcher/SandwichWatcher/job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandwich
+{
+    public class Job
+    {
+        public string status { get; set; }
+        public string filepath { get; set; }
+        public string job_id { get; set; }
+    }
+
+}
---
+++ addin/src/watcher/SandwichWatcher/SandwichWatcher/watcher.cs
@@ -0,0 +1,49 @@
+﻿#region Namespaces
+using System.IO;
+using System.Collections.Generic;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using Autodesk.Revit.DB.Events;
+
+#endregion
+
+namespace Sandwich
+{
+    public class Watcher {
+
+        public string pendingJobFolder;
+        public Queue<Job> jobQueue;
+
+        public Watcher(string watchPath) {
+            pendingJobFolder = watchPath;
+            jobQueue = JobHelper.GetPendingJobs(pendingJobFolder);
+    }
+
+        // Keeps watcher from being garbage collected
+        private static FileSystemWatcher watcher;
+
+        public void watch()
+        {
+
+            // Adds Listener for Future Jobs
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = pendingJobFolder;
+            //watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.Size;
+            watcher.Filter = "*.*";
+            watcher.EnableRaisingEvents = true;
+
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            //string pendingJobPath = e.FullPath;
+            jobQueue = JobHelper.GetPendingJobs(pendingJobFolder);
+            //_uiapp.OpenAndActivateDocument(jobRevitPath);
+            //_app.LoadFile(jobRevitPath);
+            //_uiapp.LoadFile(jobRevitPath);
+
+        }
+
+    }
+}
